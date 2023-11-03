4093c7672f6dcd6928cb56ce58dade753726686b
Eser DENIZ <srwiez@gmail.com>
2023-11-03 15:02:25 +0000

    First commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Laravel queue size check
+[![Latest Stable Version](http://poser.pugx.org/srwiez/queue-size-health-check/v)](https://packagist.org/packages/srwiez/queue-size-health-check) [![Total Downloads](http://poser.pugx.org/srwiez/queue-size-health-check/downloads)](https://packagist.org/packages/srwiez/queue-size-health-check) [![Latest Unstable Version](http://poser.pugx.org/srwiez/queue-size-health-check/v/unstable)](https://packagist.org/packages/srwiez/queue-size-health-check) [![License](http://poser.pugx.org/srwiez/queue-size-health-check/license)](https://packagist.org/packages/srwiez/queue-size-health-check) [![PHP Version Require](http://poser.pugx.org/srwiez/queue-size-health-check/require/php)](https://packagist.org/packages/srwiez/queue-size-health-check)
+![GitHub Workflow Status (with event)](https://img.shields.io/github/actions/workflow/status/SRWieZ/queue-size-health-check/test.yml?label=Tests)
+
+This package is for the people
+
+## Installation
+
+Requirement : Before installing this package, please install and configure [Laravel Health](https://github.com/spatie/laravel-health).
+
+```bash
+composer require srwiez/queue-size-health-check
+```
+
+## Usage
+
+```php
+QueueSizeCheck::new()
+    ->queue('static_cache', 3)
+    ->queue('default', 100),
+```
+
+## Contribute / Roadmap
+Hey, pull-request are welcome!
+Here is some of the things that can be improved
+- Tests
+- Github workflow to test again laravel versions
+
+## Credits
+
+Laravel queue size check was created by Eser DENIZ.
+
+## License
+
+Laravel queue size check PHP is licensed under the MIT License. See LICENSE for more information.
---
+++ src/QueueSizeCheck.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace QueueSizeCheck;
+
+use Illuminate\Support\Facades\Queue;
+use Spatie\Health\Checks\Check;
+use Spatie\Health\Checks\Result;
+
+class QueueSizeCheck extends Check
+{
+    protected array $queues = [];
+
+    public function queue(string $name, int $max_size, $connection = null): self
+    {
+        $this->queues[] = [
+            'name' => $name,
+            'connection' => $connection,
+            'max_size' => $max_size,
+        ];
+
+        return $this;
+    }
+
+    public function run(): Result
+    {
+        $queue_to_be_checked = collect($this->queues);
+
+        // Check the default queue
+        if ($queue_to_be_checked->isEmpty()) {
+            $queue_to_be_checked->push([
+                'name' => null,
+                'connection' => null,
+                'max_size' => 100,
+            ]);
+        }
+
+        $queue_to_be_checked = $queue_to_be_checked->map(function ($queue) {
+            $queue['size'] = Queue::connection($queue['connection'])->size($queue['name']);
+
+            return $queue;
+        });
+
+        $result = Result::make()->meta($queue_to_be_checked->toArray());
+
+        $queue_to_be_checked = $queue_to_be_checked
+            ->filter(fn (array $queue) => $queue['size'] > $queue['max_size']);
+
+        if ($queue_to_be_checked->isEmpty()) {
+            return $result
+                ->ok()
+                ->shortSummary('Queues sizes are ok');
+        }
+
+        $message = $queue_to_be_checked->count() === 1
+            ? 'This queue is'
+            : 'These queues are';
+
+        $message .= ' too big: '.$queue_to_be_checked->map(function (array $queue) {
+            return '"'.$queue['name'].'" ('.$queue['size'].' greater than '.$queue['max_size'].')';
+        })->join(', ', ' and ');
+
+        return $result->failed($message);
+    }
+}
