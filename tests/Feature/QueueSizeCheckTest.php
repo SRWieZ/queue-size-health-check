65ed66dc9bcfc7644f4605ddb9edd2da27a1c161
Eser DENIZ <srwiez@gmail.com>
2023-12-03 12:24:59 +0000

    well not sure it's the best way but should work

--- tests/Feature/QueueSizeCheckTest.php
+++ tests/Feature/QueueSizeCheckTest.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Notification;
+use Spatie\Health\Commands\RunHealthChecksCommand;
 use Spatie\Health\Enums\Status;
+use Spatie\Health\Facades\Health;
 use Tests\FakeQueueSizeCheck;
+
+use function Pest\Laravel\artisan;
 
 it('will determine that queue size is ok if it does not cross the maximum', function () {
     $result = FakeQueueSizeCheck::new()
@@ -21,3 +26,22 @@ it('will determine that queue size is not ok if it does cross the maximum', func
 
     expect($result->status)->toBe(Status::failed());
 });
+
+it('should not send a notification on a successful check', function () {
+    Notification::fake();
+
+    registerPassingQueueSizeCheck();
+
+    artisan(RunHealthChecksCommand::class)->assertSuccessful();
+
+    Notification::assertNothingSent();
+});
+
+function registerPassingQueueSizeCheck()
+{
+    Health::checks([
+        FakeQueueSizeCheck::new()
+            ->queue('default', 100)
+            ->fakeQueueSize(10),
+    ]);
+}
