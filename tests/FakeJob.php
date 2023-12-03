d5c57ed8da5a7513e891bb5531c7761a5c52c39c
Eser DENIZ <srwiez@gmail.com>
2023-12-03 11:38:23 +0000

    Add base test

---
+++ tests/FakeJob.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FakeJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    public $data;
+    
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+    
+    public function handle()
+    {
+        // Job logic goes here
+    }
+}
--- tests/Feature/QueueSizeCheckTest.php
+++ tests/Feature/QueueSizeCheckTest.php
@@ -1,1 +1,13 @@
 <?php
+use Illuminate\Support\Facades\Queue;
+use Tests\FakeJob;
+
+test('multiple jobs are added to the queue', function () {
+    Queue::fake();
+    
+    for ($i = 0; $i < 10; $i++) {
+        FakeJob::dispatch('data' . $i);
+    }
+    
+    Queue::assertPushed(FakeJob::class, 10);
+});
