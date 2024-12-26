817bb8e2586b0a69d63b86af2a9fddbf20c8e9ae
Okojie Davis George <binarygeotech@users.noreply.github.com>
2024-12-26 18:15:53 +0100

    fix: duplicate names (#6)
    Added default name based on the queue name(s) (#6)

--- src/QueueSizeCheck.php
+++ src/QueueSizeCheck.php
@@ -18,6 +18,11 @@ class QueueSizeCheck extends Check
             'max_size' => $max_size,
         ];
 
+        if (!$this->name) {
+            $names = array_map(fn ($queue) => $queue['name'], $this->queues);
+            $this->name = 'QueueSizeCheck_' . implode('_', $names);
+        }
+
         return $this;
     }
 
